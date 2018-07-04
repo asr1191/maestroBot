6f1bfe2a202385d4d7b3984324015bd7c58dd27f
Amal S Raj <amalsraj@cet.ac.in>
2018-07-05 01:11:29 +0530

    housekeeping

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const Dotenv = require('dotenv');
 
 Dotenv.config();
 
-const D = require('debug')('all');
-// const V = require('debug')('verbose');
+const A = require('debug')('debug:app');
+const T = require('debug')('debug:tweet');
 const Twitter = require('twitter');
 const Discord = require('discord.js');
 
@@ -14,28 +14,28 @@ const client = new Twitter({
   access_token_secret: process.env.TWITTER_TOKEN_SECRET,
 });
 
-const discordHook = new Discord.WebhookClient(process.env.VINDVEIL_ID, process.env.VINDVEIL_TOKEN);
-if (discordHook != null) {
-  D('APP: Discord Webhook connection established');
-} else {
-  D('APP: Discord Webhook initialization failed');
-}
+const discordHook = new Discord.WebhookClient(process.env.HOOK_ID, process.env.HOOK_TOKEN);
 
 function executeHook(message) {
-  D('APP: Sending message to Webhook');
+  A('Sending message to Webhook');
   discordHook.send(message);
-  D('APP: Sent message to webhook');
+  A('Sent message to webhook');
 }
 
-const stream = client.stream('statuses/filter', { follow: '854197529575137280' });
-D('APP: Running');
+const followIDs = process.env.TWITTER_IDS.split(' ').join(',');
+A('Watching Twitter IDs %s', followIDs);
+
+const stream = client.stream('statuses/filter', { follow: followIDs });
+A('Running');
 
 stream.on('data', (event) => {
-  if (event.retweeted_status == null && event.in_reply_to_status_id == null) {
-    D('TWEET:NewTweetObj: %O', event);
+  if (event.retweeted_status == null
+    && event.in_reply_to_status_id == null
+    && event.in_reply_to_user_id) {
+    T('NewTweetObj: %O', event);
 
     const embedLink = `https://twitter.com/${event.user.screen_name}/status/${event.id_str}`;
-    D('TWEET:Embed: %S', embedLink);
+    T('Embed: %s', embedLink);
     executeHook(embedLink);
   }
 });
